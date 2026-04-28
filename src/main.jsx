e0a28bb96702f029a58bd36284eb7897cadafcd0
Emdi Gemini <yougotjinx@gmail.com>
2026-04-28 21:52:40 +0800

    fix again lol

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/e-protect">
+    <BrowserRouter basename="/e-protect-system">
       <App />
     </BrowserRouter>
   </StrictMode>,
